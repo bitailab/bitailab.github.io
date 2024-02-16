c92e58c1e8edb32f609b7cd44bfa65ce0ccf97fb
haolong <haolong86@qq.com>
2024-02-16 21:41:07 +0800

    init my website

---
+++ index.search.js
@@ -0,0 +1,34 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/basics/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
